4f4ba9955ebcc28aa7f0761f7f540c29a250d5d8
osama-abdallah <osama.i.abdallah95@gmail.com>
2022-02-09 14:36:04 +0200

    task6_complete2

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ var yourName = prompt("What is your name?");
 var yourAge = prompt('How old are you?');
 
 var ageResult = (yourAge <= 0) ?
-    alert('The age you entered is incorrect \nas its less than or equal to zero') : undefined;
+alert('The age you entered is incorrect \nas its less than or equal to zero') : undefined;
 
 var yourGender = prompt('Are you a male or female?');
+
+let answerArr = [yourName, yourAge, yourGender, wantWelcome,];
+
+
 
 if (yourGender.toLowerCase() == 'male') 
 {
     var wantWelcome = confirm('Would you like to skip the welcoming message?');
-
+    
     if (wantWelcome) { undefined; }
-
+    
     else {
         alert(`welcome Mr. ${yourName}`);
     }
@@ -22,7 +26,7 @@ if (yourGender.toLowerCase() == 'male')
 
 else if (yourGender.toLowerCase() == 'female') 
 {
-
+    
     var wantWelcome = confirm('Would you like to skip the welcoming message?');
 
     if (wantWelcome) { undefined }
@@ -33,37 +37,84 @@ else if (yourGender.toLowerCase() == 'female')
 else 
 {
     var wantWelcome = confirm('Would you like to skip the welcoming message?');
-
+    
     if (wantWelcome) { undefined }
     else {
         alert(`welcome ${yourName}`);
     }
-
+    
 }
 // while (yourGender!=="male" && yourGender!=="female") {
-//    yourGender= prompt("are you male or female?");
-// }
-let question1 = askQuestion("Are you excited to see the Todo list?");
+    //    yourGender= prompt("are you male or female?");
+    // }
+    let answerArr2 = [];
 
-let question2 = askQuestion("Do you know Osama?");
-
-let question3 = askQuestion("Do you think you Osama's Todo list is going to be useful?");
-
-function askQuestion(question) 
+    let question1 = "Are you excited to see the Todo list?";
+    
+    function askQuestion1 (question) 
 {
 
     let answer = prompt(question);
 
     if (answer == "yes" || answer == "no") {
-        return answer;
+        return answerArr2.push(answer);
     } else if (answer == "") {
-        return "Invalid";
+        return answerArr2.push("Invalid");
     }
 
 }
 
-let answerArr = [yourName, yourAge, yourGender, wantWelcome, question1, question2, question3];
+let question2 = "Do you know Osama?";
+
+function askQuestion2 (question) 
+{
+
+    let answer = prompt(question);
+
+    if (answer == "yes" || answer == "no") {
+        return answerArr2.push(answer);
+    } else if (answer == "") {
+        return answerArr2.push("Invalid");
+    }
+
+}
+
+let question3 = "Do you think you Osama's Todo list is going to be useful?";
+
+function askQuestion3 (question) 
+{
+
+    let answer = prompt(question);
+
+    if (answer == "yes" || answer == "no") {
+        return answerArr2.push(answer);
+    } else if (answer == "") {
+        return answerArr2.push("Invalid");
+    }
+
+
+}
+askQuestion1(question1);
+askQuestion2(question2);
+askQuestion3(question3);
+
+// function askQuestion(question) 
+// {
+
+//     let answer = prompt(question);
+
+//     if (answer == "yes" || answer == "no") {
+//         return answer;
+//     } else if (answer == "") {
+//         return "Invalid";
+//     }
+
+// }
+
 
 for(let i = 0 ; i<answerArr.length ; i++){
     console.log(answerArr[i]);
+}
+for(let i = 0 ; i<answerArr2.length ; i++){
+    console.log(answerArr2[i]);
 }
